db6f9094e57167cef0e530964ecfca93e1160277
annayurquina <annayurquina@gmail.com>
2023-03-08 03:17:55 -0300

    arreglado pequeñso detalles de task 2: responsive en checkboxes, search y boton search. Arreglado el problema de pastEvents y upcomingEvents, ahora se filtra por fecha

---
+++ js/upcoming.js
@@ -0,0 +1,33 @@
+let cardEvents = []
+for (let event of data.events) {
+  if (event.date>=data.currentDate) {
+    let card = `
+    <div class="col-12 col-md-6 col-lg-4">
+      <div class="card text-center">
+        <figure>
+          <img class="img-card img-fluid mt-3" src=${event.image} alt=${event.name}>
+        </figure>
+        <div>
+          <h3>${event.name}</h3>
+          <p><span>Date:</span> ${event.date}</p>
+          <h5><span>Description:</span> ${event.description}</h5>
+          <p><span>Category:</span> ${event.category}</p>
+          </div>
+          <div class="mycard-footer">
+          <p><span>Price:</span> ${event.price}</p>
+          <a href="./detail.html" class="btn-mas">Ver más...</a>
+        </div>
+      </div>
+    </div>
+    `
+    console.log(card)
+    cardEvents.push(card)
+  }
+}
+
+function printCards() {
+  let mycontainer = document.querySelector("#mycontainer-cards")
+  mycontainer.innerHTML=cardEvents.join('')
+}
+
+printCards()
