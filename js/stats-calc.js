8ad103ac942373586e9ec43489a7dd36928ebbfa
annayurquina <annayurquina@gmail.com>
2023-03-21 20:04:01 -0300

    arreglado calculos de primera tabla, ahora es porcentaje. Cambio de estilo en las tablas

--- js/stats-calc.js
+++ js/stats-calc.js
@@ -1,19 +1,21 @@
 function sortarrayEvents(typeOrder, arrayObjEvents) {
   salida=[]
   switch (typeOrder) {
-    case 0://ordena por mayor a menor asistencia
-      salida = salida.concat([...arrayObjEvents].sort((event1, event2) => event2.assistance - event1.assistance))
-      //console.log("🚀 ~ file: stats-api.js:16 ~ sortarrayEvents ~ salida:", salida)
+    case 0://ordena por mayor a menor porcentaje de asistencia
+      salida = salida.concat([...arrayObjEvents].sort((event1, event2) => ((100 * event2.assistance) / event2.capacity) - ((100 * event1.assistance) / event1.capacity)))
+      //console.log("🚀 ~ mayor % asistencia:", salida)
       break
-    case 1://ordena de menor a mayor asistencia
-      salida = salida.concat([...arrayObjEvents].sort((event1,event2)=>event1.assistance - event2.assistance))
-      //console.log("🚀 ~ file: stats-calc.js:32 ~ sortarrayEvents ~ salida:", salida)
+    case 1://ordena de menor a mayor porcentaje de asistencia
+      salida = salida.concat([...arrayObjEvents].sort((event1, event2) => ((100 * event1.assistance) / event1.capacity) - ((100 * event2.assistance) / event2.capacity)))
+      //console.log("🚀 ~ menor % asistencia:", salida)
+      
       break
     case 2: //ordena de mayor a menor capacidad
-      salida=salida.concat([...arrayObjEvents].sort((event1, event2) => event2.capacity - event1.capacity))
+      salida = salida.concat([...arrayObjEvents].sort((event1, event2) => event2.capacity - event1.capacity))
+      //console.log("🚀 ~ mayo a menor capacidad", salida)
       break
     
-    case 3: //ordena de mayor a menor porcentanje
+    case 3: //ordena de mayor a menor porcentaje
       salida = salida.concat(arrayObjEvents.sort((event1, event2) => event2.percentage - event1.percentage))
       break
     default:
--- js/stats-templates.js
+++ js/stats-templates.js
@@ -1,11 +1,16 @@
 function templateTable1(arrayRows) {
   let salida=''
   for (let myrow of arrayRows) {
+    let porcentaje_high = (myrow.high_assistance.assistance * 100) / myrow.high_assistance.capacity
+    let porcentaje_low = (myrow.low_assistance.assistance * 100) / myrow.low_assistance.capacity 
     salida = salida.concat(`
       <tr>
         <td>${myrow.high_assistance.name}</td>
+        <td>${porcentaje_high.toFixed(2)}%</td>
         <td>${myrow.low_assistance.name}</td>
+        <td>${porcentaje_low.toFixed(2)}%</td>
         <td>${myrow.high_capacity.name}</td>
+        <td>${myrow.high_capacity.capacity}</td>
       </tr>
     `)
   }
